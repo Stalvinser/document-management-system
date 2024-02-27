c7db9901c82315c951994d62d2c6dc1b99be38f6
Stalvinser <alterwayt@yandex.ru>
2024-02-28 00:16:47 +0300

    Security config basic

---
+++ src/main/java/com/astarus/documentmanagementsystem/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.astarus.documentmanagementsystem.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/astarus/documentmanagementsystem/security/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.astarus.documentmanagementsystem.security;
+
+import com.astarus.documentmanagementsystem.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+    private final AppUserService appUserService;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+}
